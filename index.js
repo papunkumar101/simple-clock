ddb234e57c80cb5b598b6e10bf9505d850a1ca39
Rodrigo Gameiro <rodrigo2014conceicao@gmail.com>
2019-09-03 18:59:33 +0100

    Polished the code... Just a bit.

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
-function updateClock(date) {
-    if(typeof(date) != typeof(new Date())) {
-        console.debug(updateClock.name + " was called without a Date???");
-        return;
-    }
-
+function updateClock() {
+    
+    let date = new Date(Date.now());
     let clock = document.getElementById("clock");
+
     clock.textContent = date.getHours() + ":" + date.getMinutes();
 
 }
 
-function updateDate(date) {
-    if(typeof(date) != typeof(new Date())) {
-        console.debug(updateDate.name + " was called without a Date???");
-        return;
-    }
+function updateDate() {
+
+    let date = new Date(Date.now());
+    let day = document.getElementById("day");
 
     let weekDict = {
         0: "Monday",
@@ -23,7 +20,7 @@ function updateDate(date) {
         4: "Friday",
         5: "Saturday",
         6: "Sunday"
-    }
+    };
 
     let monthDict = {
         0: "January",
@@ -38,23 +35,21 @@ function updateDate(date) {
         9: "October",
         10: "November",
         11: "December"
-    }
-
-    let day = document.getElementById("day");
+    };
 
     day.textContent = 
     weekDict[date.getDay()] + ", " +
     monthDict[date.getMonth()] + " " +
     date.getDate();
 
-
 }
 
 window.onload = function() {
-    let date = new Date(Date.now());
-    updateClock(date);
-    updateDate(date);
 
-    setInterval(updateClock, 500, date);
-    setInterval(updateDate, 1000 * 60, date);
+    updateClock();
+    updateDate();
+
+    setInterval(updateClock, 1000);
+    setInterval(updateDate, 1000 * 60);
+
 }
